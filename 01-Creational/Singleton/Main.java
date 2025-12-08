0f4de5e5981d051f24ba620cba7a0f552f8e1bff
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-08 11:43:31 +0300

    update: enhance Singleton demo to show data parameter usage in getInstance calls

--- 01-Creational/Singleton/Main.java
+++ 01-Creational/Singleton/Main.java
@@ -3,24 +3,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Singleton Pattern Demo ===\n");
 
-        // First call - instance will be created
+        // First call - instance will be created with data
         System.out.println("Calling getInstance() for the first time:");
-        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton1 = Singleton.getInstance("Initial Data");
         System.out.println("singleton1 hash: " + singleton1.hashCode());
+        System.out.println("singleton1 data: " + singleton1.getData());
 
         System.out.println();
 
-        // Second call - same instance will be returned
+        // Second call - same instance will be returned (data parameter ignored)
         System.out.println("Calling getInstance() for the second time:");
-        Singleton singleton2 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance("Different Data");
         System.out.println("singleton2 hash: " + singleton2.hashCode());
+        System.out.println("singleton2 data: " + singleton2.getData());
 
         System.out.println();
 
         // Third call - still the same instance
         System.out.println("Calling getInstance() for the third time:");
-        Singleton singleton3 = Singleton.getInstance();
+        Singleton singleton3 = Singleton.getInstance("Another Data");
         System.out.println("singleton3 hash: " + singleton3.hashCode());
+        System.out.println("singleton3 data: " + singleton3.getData());
 
         System.out.println();
 
