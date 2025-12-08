49505306a1d16a2ab280d90ded609c4fa02fc78d
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-08 08:21:47 +0300

    refactor: implement Vehicle interface and corresponding factory classes

--- 01-Creational/FactoryMethod/FactoryMethod.java
+++ 01-Creational/FactoryMethod/FactoryMethod.java
@@ -1,3 +1,54 @@
-public class FactoryMethod {
-    
+interface Vehicle {
+    String getType();
+}
+
+// Concrete Products
+class Car implements Vehicle {
+    @Override
+    public String getType() {
+        return "Car";
+    }
+}
+
+class Bike implements Vehicle {
+    @Override
+    public String getType() {
+        return "Bike";
+    }
+}
+
+class Truck implements Vehicle {
+    @Override
+    public String getType() {
+        return "Truck";
+    }
+}
+
+// Abstract Creator
+abstract class VehicleFactory {
+    // Factory method that subclasses must implement
+    abstract Vehicle createVehicle();
+}
+
+// Concrete Creators
+class CarFactory extends VehicleFactory {
+    @Override
+    Vehicle createVehicle() {
+        // The factory creates its own special product here and returns
+        return new Car();
+    }
+}
+
+class BikeFactory extends VehicleFactory {
+    @Override
+    Vehicle createVehicle() {
+        return new Bike();
+    }
+}
+
+class TruckFactory extends VehicleFactory {
+    @Override
+    Vehicle createVehicle() {
+        return new Truck();
+    }
 }
