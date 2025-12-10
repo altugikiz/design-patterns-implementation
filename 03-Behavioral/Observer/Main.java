13bee193c89e93128c6c20ddc16118faedebd826
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-10 07:23:22 +0300

    add: enhance Main class with Observer pattern demo functionality

--- 03-Behavioral/Observer/Main.java
+++ 03-Behavioral/Observer/Main.java
@@ -1,3 +1,27 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Observer Pattern Demo (YouTube) ===\n");
 
+        // 1. Create the channel
+        YoutubeChannel myChannel = new YoutubeChannel("The Walking Dead");
+
+        // 2. Create subscribers
+        Subscriber user1 = new Subscriber("Darly Dixon");
+        Subscriber user2 = new Subscriber("Rick Grimes");
+        Subscriber user3 = new Subscriber("Negan");
+
+        // 3. Users subscribe to the channel
+        myChannel.subscribe(user1);
+        myChannel.subscribe(user2);
+        myChannel.subscribe(user3);
+
+        // 4. Upload video (Notification should be sent automatically)
+        myChannel.uploadVideo("How Does the Observer Pattern Work?");
+
+        // 5. One person unsubscribes and we upload a new video
+        System.out.println("\n--- Negan is unsubscribing ---");
+        myChannel.unsubscribe(user2);
+
+        myChannel.uploadVideo("Who killed Glenn?");
+    }
 }
