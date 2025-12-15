302036922456479bc2ea03757c8454d667dfe688
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-15 09:03:23 +0300

    add: implement Light class with turnOn and turnOff methods

---
+++ 03-Behavioral/Command/Light.java
@@ -0,0 +1,9 @@
+public class Light {
+    public void turnOn() {
+        System.out.println("Lamp: Lights ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Lamp: Lights OFF");
+    }
+}
