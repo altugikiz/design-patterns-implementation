bba3aab47d8b087c0ce489b84a044b74b1a777cf
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-16 08:28:55 +0300

    add: implement File class as part of Composite pattern structure

---
+++ 02-Structural/Composite/File.java
@@ -0,0 +1,11 @@
+public class File extends FileSystemComponent {
+
+    public File(String name) {
+        super(name);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("File: " + name);
+    }
+}
