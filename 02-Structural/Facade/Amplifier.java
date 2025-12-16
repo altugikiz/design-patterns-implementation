60a35c514bdbf0b6c65a7b5a671292f5b2110c6b
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-16 07:14:50 +0300

    add: implement Amplifier class with on, setVolume, and off methods

---
+++ 02-Structural/Facade/Amplifier.java
@@ -0,0 +1,13 @@
+public class Amplifier {
+    public void on() {
+        System.out.println("Amplifier turned on.");
+    }
+
+    public void setVolume(int level) {
+        System.out.println("Volume set to " + level + ".");
+    }
+
+    public void off() {
+        System.out.println("Amplifier turned off.");
+    }
+}
