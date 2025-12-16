6e11698224a67500a4857d0eebe31acdaef3610c
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-16 07:17:16 +0300

    add: implement TheaterLights class with dim and on methods

---
+++ 02-Structural/Facade/TheaterLights.java
@@ -0,0 +1,9 @@
+public class TheaterLights {
+    public void dim(int level) {
+        System.out.println("Lights dimmed to " + level + "%.");
+    }
+
+    public void on() {
+        System.out.println("Lights fully turned on.");
+    }
+}
