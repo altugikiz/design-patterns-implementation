01233a648f91f6eeee9972daacd3a9f1126df369
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-12-16 07:16:01 +0300

    add: implement Projector class with on, setInput, and off methods

---
+++ 02-Structural/Facade/Projector.java
@@ -0,0 +1,13 @@
+public class Projector {
+    public void on() {
+        System.out.println("Projector turned on.");
+    }
+
+    public void setInput(String input) {
+        System.out.println("Input source selected: " + input);
+    }
+
+    public void off() {
+        System.out.println("Projector turned off.");
+    }
+}
